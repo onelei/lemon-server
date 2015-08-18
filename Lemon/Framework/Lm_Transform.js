3c24a66c9c6b79d0ee9e9ace80341e7e73546939
onelei <ahleiwolong@163.com>
2015-08-18 18:53:38 +0800

    add transform,delete *.~

---
+++ Lemon/Framework/Lm_Transform.js
@@ -0,0 +1,19 @@
+/**
+ * Created by onelei on 15-8-18.
+ *
+ * this is a message transform script;
+ */
+
+var Lm_Transform= function()
+{
+
+};
+
+module.exports = Lm_Transform;
+
+// parse json in message;
+Lm_Transform.parse = function(msg)
+{
+    console.log(msg);
+};
+
